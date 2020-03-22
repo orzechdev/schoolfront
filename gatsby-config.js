5aff2d8b6750c7e5c9e0cb137a5dcf61d3360062
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-03-22 15:56:05 +0100

    Add schol webpage description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby School Webpage Starter`,
+    description: `Kick off your next, great Gatsby school webpage project with this starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@orzechdev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
