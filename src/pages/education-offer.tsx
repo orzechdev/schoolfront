25abbb02b4fe2d21b2a0131f1f1e238f145881f4
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-04-17 02:30:19 +0200

    Create education offer page

---
+++ src/pages/education-offer.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const EducationOfferPage = () => {
+  const educationOffer = useStaticQuery(graphql`
+    query EducationOfferQuery {
+      allEducationOfferJson {
+        edges {
+          node {
+            studyField
+            summary
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Education offer" />
+      <h1>Education offer</h1>
+      <p>
+        We offer three different specializations. Each one consists of courses
+        with gaining field knowledge as well as performing practical trainings.
+      </p>
+      <ul>
+        {educationOffer.allEducationOfferJson.edges.map(
+          ({ node }, index: number) => (
+            <li key={index}>
+              <h2>{node.studyField}</h2>
+              <p>{node.summary}</p>
+            </li>
+          )
+        )}
+      </ul>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export default EducationOfferPage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,18 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h2>Check out</h2>
+    <ul>
+      <li>
+        <Link to="/teaching-staff/">Teaching staff</Link>
+      </li>
+      <li>
+        <Link to="/education-offer/">Education offer</Link>
+      </li>
+      <li>
+        <Link to="/page-2/">Go to page 2</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
